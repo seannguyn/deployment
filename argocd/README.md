bfe763064fba6200e2a6662a5dd8d385275c1fa4
Sean Nguyen <30516148+seannguyn@users.noreply.github.com>
2025-03-04 21:05:09 +1100

    stuff

--- argocd/README.md
+++ argocd/README.md
@@ -10,11 +10,11 @@ kubectl delete -f argocd/app.yaml
 argocd login localhost:8080 --insecure --username admin --password $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 
 # PASS
-argocd app sync serverless --revision 4ebc9415ae66ed9149c37cebe4c85b889953c2b6 --dry-run 2>&1
+argocd app sync serverless --dry-run --revision 4ebc9415ae66ed9149c37cebe4c85b889953c2b6
 echo $?
 
 # FAIL
-argocd app sync serverless --revision ca0c48813545f74cc49c997aca8ade17951c1ad6 --dry-run 2>&1
+argocd app sync serverless --dry-run --revision 4ebc9415ae66ed9149c37cebe4c85b889953c2b6
 echo $?
 
 ```
--- argocd/script.sh
+++ argocd/script.sh
@@ -1,6 +1,6 @@
 APP_NAME="serverless"
 COMMIT_HASH_PASS="4ebc9415ae66ed9149c37cebe4c85b889953c2b6"
-COMMIT_HASH_FAIL="89584c975086df31478ec9864f2805988ed2618e"
+COMMIT_HASH_FAIL="b58b8e58d7f097f10dc7d56a4147de84543fa7b7"
 
 # Run ArgoCD sync with dry-run and capture YAML output
 OUTPUT=$(argocd app sync "$APP_NAME" --revision "$COMMIT_HASH_FAIL" --dry-run --server-side -o yaml)
@@ -14,7 +14,11 @@ ERROR_MESSAGE=$(echo "$OUTPUT" | yq e '.status.operationState.message' -)
 # Check if sync failed
 if [[ "$SYNC_PHASE" != "Succeeded" ]]; then
     echo "❌ ArgoCD dry-run sync failed for commit $COMMIT_HASH_FAIL"
+    echo
     echo "Error Message: $ERROR_MESSAGE"
+    echo
+    # This will display full output for debugging
+    argocd app sync $APP_NAME --revision $COMMIT_HASH_FAIL --dry-run --server-side
     exit 1
 else
     echo "✅ ArgoCD dry-run sync succeeded for commit $COMMIT_HASH_FAIL"
